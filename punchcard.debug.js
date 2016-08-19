bba3335912503f7f9d29d9a4d2be0993bdd4abf2
Lutz Rosema <terabaud@gmail.com>
2016-08-19 16:55:12 +0200

    ...

--- punchcard.debug.js
+++ punchcard.debug.js
@@ -2,7 +2,7 @@ window.onload=function(s,e,x,y,c,o,d3){
 	o="textContent";
 	d3=document.querySelectorAll('script');
 	for(c=d3.length;c--;){
-		if (/source/.test(d3[c][o]))console.log(d3[c][o]);
+		console.log("code", d3[c][o]);
 		if(/\u1680/.test(d3[c][o])){
 			console.log("test");
 			s=(d3[c][o].replace(/[^\u1680\u0020]/g,"").match(/  \u1680 \u1680{4}([ \u1680\n]+)  \u1680 \u1680{4}/)||[""])[0];
